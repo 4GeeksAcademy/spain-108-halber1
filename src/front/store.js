a88284e94e2d0c0df32181bc81286ad53966d951
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-06-13 14:22:08 +0000

    Proyecto limpio para continuar

--- src/front/store.js
+++ src/front/store.js
@@ -1,40 +1,15 @@
-
-const host = 'https://playground.4geeks.com/todo';
-const user = 'Halber1';
-
-
-
-
-
-
-
-
-export const initialStore=()=>{
-  return{
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+export const initialStore = () => {
+  return {
+    contacts: [],
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){ 
-    case 'add_task':
-      const { id,  color } = action.payload
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+  switch (action.type) {
+    case "contacts":
+      return { ...store, contacts: action.payload };
+
     default:
-      throw Error('Unknown action.');
-  }    
+      throw Error("Unknown action.");
+  }
 }
