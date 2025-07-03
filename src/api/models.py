466deb520ce2e62ed51e0be2b5bb10183df1417e
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-07-03 19:43:07 +0000

    En progreso

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return f"user: {self.id} - email: {self.email}"
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # do not serialize the password, its a security breach
+        return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active,
+                "first_name": self.first_name,
+                "last_name": self.last_name,}
+
+    class Posts(db.Model):
+        _tablename_ = 'post'
+        id = db.Colum(db.Integer, primary_key=True)
+        title = db.Colum(db.String, nullable=False)
+        description = db.Colum(db.String)
+        body = db.Colum(db.String)
+        date = db.Colum()
+        image_url = db.Colum()
+        
+
+    
