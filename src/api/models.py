b39216abf0a4fe3d7a0f6468838a5e3bba99b4da
Alberto Fernández <albertofernandezfernandez01@gmail.com>
2025-07-08 12:44:20 +0000

    tablas-falta enpoints

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,8 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String, nullable=True)
+    last_name = db.Column(db.String, nullable=True)
 
     def __repr__(self):
         return f"user: {self.id} - email: {self.email}"
@@ -19,7 +21,13 @@ class Users(db.Model):
                 "email": self.email,
                 "is_active": self.is_active,
                 "first_name": self.first_name,
-                "last_name": self.last_name, }
+                "last_name": self.last_name,
+                "title": self.title,
+                "description": self.description,
+                "body": self.body,
+                "date": self.date,
+                "image_url": self.image_url,
+                "user_to": self.user_to}
 
 
 class Posts(db.Model):
@@ -38,6 +46,69 @@ class Posts(db.Model):
         return f'<User {self.id}>'
 
 
-# class Media(db.Model):
-#   _tablename_ = 'media'
-#    id = db.Column
+class Media(db.Model):
+    _tablename_ = 'media'
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(
+        db.Enum('image', 'video', 'audio', name='media_type'), nullable=False)
+    url = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_to = db.relatrionship('Users', back_populates="media_to", lazy='select')
+
+
+class Comments(db.Model):
+    _tablename_ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_to = db.relationship('Users', back_populates="coments_to", lazy='select')
+    post_to = db.relationship('User')
+
+
+class Followers(db.Model):
+    _tablename_ = 'folowers'
+    id = db.Column(db.Integer, primary_key=True)
+    following_id = db.Column('user.id')
+    follower_id = db.Column('user.id')
+
+
+class CharacterFavorites(db.Model):
+    _tablename_ = 'characterfavorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+
+
+class Characters(db.Model):
+    _tablename_ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    height = db.Column(db.String)
+    mass = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    birth_year = db.Column(db.String)
+    gender = db.Column(db.String)
+         
+class PlanetFavorites(db.Model):
+    _tablename_ = 'planetfavorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    
+
+class Planets(db.Model):
+    _tablename_ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    diameter = db.Column(db.String)
+    rotation_period = db.Column(db.String)
+    orbital_period = db.Column(db.String)
+    gravity = db.Column(db.String)
+    population = db.Column(db.String)
+    climate = db.Column(db.String)
+    terrain = db.Column(db.String)
